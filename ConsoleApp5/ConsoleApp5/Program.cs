aa3a07cd88b49374fc9589fbd5d5cdc6cc3a2596
CeyhunYadigarli <ceyhunyadigarov248@gmail.com>
2024-09-21 00:15:00 +0400

    Project1
    Ededin tek eded oldugunu bildiren alqoritm

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eded daxil edin");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            if(a%2==0)
+            {
+                Console.WriteLine("Cut ededdir");
+            }
+            else
+            {
+                Console.WriteLine("Tek ededdir");
+            }
+        }
+    }
+}
